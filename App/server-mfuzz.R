034873a4346b7b8e74290417194405e138301aa0
margauxhaering <margauxhaering@orange.fr>
2021-01-13 11:14:38 +0100

    update loop

--- App/server-mfuzz.R
+++ App/server-mfuzz.R
@@ -83,11 +83,12 @@ observeEvent(input$inertiaclass,{   # when a filter of low count genes is set
     mfdata.s <- standardise(mfdata)
     m1 <- mestimate(mfdata.s)
     cent <- input$inertiaclass
-    i=0
-    for (i in 0:9){
-      N_cl<- mfuzz(mfdata.s, centers=cent, m = m1)
-      i = i + 1
-    }
+    N_cl<- mfuzz(mfdata.s, centers=cent, m = m1)
+    N_cl<- mfuzz(mfdata.s, centers=cent, m = m1)
+    N_cl<- mfuzz(mfdata.s, centers=cent, m = m1)
+    N_cl<- mfuzz(mfdata.s, centers=cent, m = m1)
+    N_cl<- mfuzz(mfdata.s, centers=cent, m = m1)
+
     ov <- overlap(N_cl)
     
   }
